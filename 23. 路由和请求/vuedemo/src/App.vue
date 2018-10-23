65bdf3dac921ec2d90cb7e247d9757e9fa8cf120
dongsijeong <18640947990@163.com>
2018-10-23 23:17:32 +0800

    1023 commit

---
+++ 23. 路由和请求/vuedemo/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id="app">
+        <ul>
+            <!-- <li>
+                <a href="/">Home</a>
+            </li>
+            <li>
+                <a href="/helloworld">HelloWorld</a>
+            </li> -->
+            <li>
+                <router-link to="/">Home</router-link>
+            </li>
+            <li>
+                <router-link to="helloworld">HelloWorld</router-link>
+            </li>
+        </ul>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'app',
+    data () {
+        return {
+        }
+    },
+    methods: {
+    }
+}
+</script>
+
+<style>
+</style>
